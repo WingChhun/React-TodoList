1f29f5c086aff75b87e50815444a95d9d1dd6f46
WingChhun <capt.wing.chhun@gmail.com>
2018-05-29 13:22:14 -0700

    Added /create route and /clear or dropAll routes, tested in postman working

--- routes/api.js
+++ routes/api.js
@@ -15,15 +15,60 @@ created:Date (created by itself)
 
 //Todo: @ROUTE /api
 //TODO: @PURPOSE: send all users, for now send test
-//TODO: @ACCESS :PUBLIC
+//TODO: @ACCESS :PUBLIC - WORKING
 router.get("/", (req, res) => {
-    res.json({msg: "Connected to API"});
+    // res.json({msg: "Connected to API"});
+    DB_TODOS
+        .find({})
+        .then((users) => {
+            res.json(users);
+            DB_TODOS.save();
+        })
+        .catch(err => console.log("Error at /api", err));
 });
 
 //TODO: @ROUTE: /api/create
 //TODO: @PURPOSE: create a new Todo
+//TODO: @ACCESS :PUBLIC - WORKING
+router.post('/', (req, res) => {
+
+    /*
+ * {
+ *  task:String,
+ * completed:boolean,
+ * created:date(default)
+ * }
+*/
+
+    const newTodo = {
+        task: req.body.task,
+        completed: true,
+        created: Date.now()
+    };
+    DB_TODOS
+        .create(newTodo)
+        .then((Todo) => res.json(Todo))
+        .catch(err => console.log(err));
+
+});
+
+//TODO: @ROUTE: /api/update
+//TODO: @PURPOSE: create a new Todo
 //TODO: @ACCESS :PUBLIC
-router.post('/create')
+router.post("/update", (req, res) => {});
+//TODO: @ROUTE: /api/create
+//TODO: @PURPOSE: create a new Todo
+//TODO: @ACCESS :PUBLIC
+router.post("/delete", (req, res) => {});
+
+//TODO: @ROUTE: /api/clear
+//TODO: @PURPOSE: clear all todos
+//TODO: @ACCESS :PUBLIC - WORKING
+router.post("/clear", (req, res) => {
+    DB_TODOS
+        .remove()
+        .then(() => res.json({msg: "cleared all"}).catch(e => console.log(e)))
+});
 
 //TODO: export router
 module.exports = router;
