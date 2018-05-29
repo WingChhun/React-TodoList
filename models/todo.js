ba03ea38d30bbd9d3726dc73edb4d8aeaad14031
WingChhun <capt.wing.chhun@gmail.com>
2018-05-28 21:55:34 -0700

    Todo.js model created

---
+++ models/todo.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//TODO: Create a schema for todo post
+//* Task : hold task as a string, required
+//* completed: boolean for mark status
+//* created : retrieve the date *
+const TodoSchema = new Schema({
+
+    task: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type:Boolean,
+        required: true
+    },
+    created: {
+        type: Date,
+        Date: Date.now
+    }
+
+});
+
+//! - Export Schema as Todo
+module.exports = Todo = mongoose.model("todos", TodoSchema);
