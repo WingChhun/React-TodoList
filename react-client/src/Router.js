4b9635b15c359a3fc93b9c54693c0ae7d2282ba8
WingChhun <capt.wing.chhun@gmail.com>
2018-05-29 20:48:51 -0700

    Create Router

---
+++ react-client/src/Router.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+
+//CSS
+import "./sass/main.css";
+//TODO: Import components
+import Main from "./container/Main";
+
+class Router extends Component {
+
+    componentWillMount()
+    {
+        //Connect to API
+        document.title = "MERN - Todo List";
+    }
+    render()
+    {
+
+        return (
+
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
+
+                <Switch>
+
+                    <Route exact path="/" component={Main}/>
+                </Switch>
+
+            </BrowserRouter>
+
+        );
+    }
+}
+
+export default Router;
