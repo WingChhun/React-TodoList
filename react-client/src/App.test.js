447dbb09a02856797071faa5f0bbeda716794e24
WingChhun <capt.wing.chhun@gmail.com>
2018-05-30 11:22:12 -0700

    Removed initial test

--- react-client/src/App.test.js
+++ react-client/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import {connectAPI} from "./jest_tests/main.js";
+
+// it('renders without crashing', () => {     const div =
+// document.createElement('div');     ReactDOM.render(         <App/>, div);
+// ReactDOM.unmountComponentAtNode(div); });
+
+//TODO: Tests to test API
+describe("Connects to express backend", () => {
+    it("should return an Array objects", () => {
+        //* connect to /api/get
+        const stringURL = "/api";
+        expect(connectAPI(stringURL)).toBe([]);
+
+    })
+
+}); //end describe
