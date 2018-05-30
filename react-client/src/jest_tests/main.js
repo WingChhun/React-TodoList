44057b5ef6bd3818ab2dd113c68e8463c2dc236d
WingChhun <capt.wing.chhun@gmail.com>
2018-05-30 11:22:52 -0700

    Added dummy tests, not priority

---
+++ react-client/src/jest_tests/main.js
@@ -0,0 +1,10 @@
+// //TODO: test API cases
+// export const connectAPI = (stringURL) => {
+//     let todoArr;
+//     fetch(stringURL)
+//         .then(todos => console.log(todos.json()))
+//         .catch(e => "error");
+
+// }
+
+
