f99410d18a592c9d8e64a9ca6092d5b310c01e47
WingChhun <capt.wing.chhun@gmail.com>
2018-06-01 12:33:53 -0700

    Logic to change status of background image

--- react-client/src/container/Main.js
+++ react-client/src/container/Main.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import styled from "styled-components";
 import "../sass/main.css";
 
 //* Import components up here
@@ -7,7 +8,12 @@ import InputBox from "../components/InputBox";
 import TodoList from "../components/Todo/TodoList";
 import Quote from "../components/Quote";
 import Footer from "../components/Footer";
+
 //TODO: Main class, main components here
+import evening from "../img/evening.jpg";
+import afternoon from "../img/afternoon.jpg";
+import morning from "../img/morning.jpg";
+
 class Main extends Component
 {
 
@@ -20,25 +26,59 @@ class Main extends Component
         todos: [],
         quote: {},
         time: "XX:XX AM",
-        seconds: 0
+        seconds: 0,
+        hours: 0,
+        minutes: 0,
+        morning: false,
+        afternoon: false,
+        evening: false,
+        backgroundImg: ""
     };
 
     //* Lifecycle Methods
     //* ComponentWillMount -> run updateTime
-    componentWillMount()
+    async componentWillMount()
     {
         this._updateTime();
+        //*If Midnight, clear the database
+        if (this.state.hours === 24 && this.state.minutes === 0 && this.state.seconds === 0) {
+
+            //TODO: make request to "/api/clear" clear todos
+            const res = await fetch("/api/clear", {method: "POST"});
+            const complete = await res.json();
+            //*if made request, clear todos
+            if (complete) {
+                this.setState({
+                    ...this.state,
+                    todos: []
+                });
+            }
+
+        }
+
     }
     async componentDidMount()
     {
         // Connect to API const res = await fetch("/api") const todos = await
         // res.json(); this.setState({     ...this.state,
-
+        const {hours, minutes} = this.state;
+        let status = 0;
         //* setInterval on window to change time
         window.setInterval(() => {
             this._updateTime();
             this._getSeconds();
         }, 1000);
+        //*Set morning, evneing, img, and bakcgroundImg
+        if (hours > 4 && hours < 12) {
+            status = 1;
+        } else if (hours >= 12 && hours < 18) {
+            //*afternoon
+            status = 2;
+        } else { //*evening
+            status = 3;
+        }
+        this._updateBackground(status);
+
     }
 
     _updateTodos = (todos) => {
@@ -53,7 +93,7 @@ class Main extends Component
     {
         const {_updateTime} = this._updateTime;
         return (
-            <div className="main">
+            <Background className="main">
                 <Header Time={this.state.time}/> {/*TodoInput*/}
                 {/*TodoInput*/}
                 <InputBox _updateTodos ={this._updateTodos}/>
@@ -69,7 +109,7 @@ class Main extends Component
                     quote={this.state.quote}/>
                 <Footer/>
 
-            </div>
+            </Background>
 
         );
 
@@ -112,10 +152,14 @@ class Main extends Component
     _getSeconds = () => {
         const event = new Date();
         const seconds = event.getSeconds();
+        const minutes = event.getMinutes();
+        const hours = event.getHours();
         //* updateSeconds
         this.setState({
             ...this.state,
-            seconds
+            seconds,
+            minutes,
+            hours
         });
     }
     //TODO: Function to update this.state.quote
@@ -138,6 +182,57 @@ class Main extends Component
         console.log("Success update single todo")
         console.log(`this.state.todos[${index}] = `, this.state.todos[index]);
     }
+
+    /*
+     TODO: Functions that involve changing CSS,
+     TODO: change Background image depending on time
+    */
+    //* Change image depending on this.state.hours
+    _updateBackground = (status) => {
+
+        const morningImg = "https://images.unsplash.com/photo-1503038324980-f38756a54c7d?ixlib=rb-0.3.5&ixid" +
+                "=eyJhcHBfaWQiOjEyMDd9&s=a8fefc558d1d35dc670dafed1b02c537&auto=format&fit=crop&w=" +
+                "1350&q=80";
+        const afternoonImg = "https://images.unsplash.com/photo-1516893623281-98535aaa2205?ixlib=rb-0.3.5&ixid" +
+                "=eyJhcHBfaWQiOjEyMDd9&s=f38c904c2615659c0bf62b3e18457b5a&auto=format&fit=crop&w=" +
+                "1350&q=80";
+        const eveningImg = "https://images.unsplash.com/photo-1515780010305-0d4bbd149cda?ixlib=rb-0.3.5&ixid" +
+                "=eyJhcHBfaWQiOjEyMDd9&s=290520babcf3a21315f4bf2899dc044f&auto=format&fit=crop&w=" +
+                "1350&q=80";
+        let backgroundImg = "";
+        //*Logic change state change image
+        switch (status) {
+            case 1:
+                backgroundImg = morningImg;
+                break;
+            case 2:
+                backgroundImg = afternoonImg;
+                break;
+            case 3:
+                backgroundImg = eveningImg;
+                break;
+            default:
+                break;
+        }
+        //*set image
+        this.setState({
+            ...this.state,
+            backgroundImg
+        });
+    }
+
 }
 
 export default Main;
+//*styled component *backgroundImg
+//*chose background image
+
+export const Background = styled.div `
+  background: url(${afternoon}) no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  max-height:90%;
+`;
+console.log(Background);
